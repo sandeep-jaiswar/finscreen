89dd8d4203c00ce855c9d83418478832c7e3ec99
sandeep-jaiswar <jaiswarsandeep119@gmail.com>
2024-10-19 15:22:08 +0530

    feat: add yahoo finance

--- finscreen/views.py
+++ finscreen/views.py
@@ -1,10 +1,10 @@
 from django.http import JsonResponse
-from nsetools import Nse
+import yfinance as yf
 
 def get_stock_data(request, symbol):
-    nse = Nse()
     try:
-        stock_data = nse.get_quote(symbol)
-        return JsonResponse(stock_data, safe=False)
+        stock = yf.Ticker(symbol)
+        stock_info = stock.info
+        return JsonResponse(stock_info, safe=False)
     except Exception as e:
         return JsonResponse({'error': str(e)}, status=400)
